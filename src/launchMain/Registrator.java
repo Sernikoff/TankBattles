e0c3c24b81eb3f22d2443c7e2d0fa6ac55de0050
Sernikoff <sernik@i.ua>
2016-07-16 17:03:42 +0300

    Registrator

---
+++ src/launchMain/Registrator.java
@@ -0,0 +1,51 @@
+package launchMain;
+
+import java.io.*;
+import java.util.LinkedList;
+
+public class Registrator {
+    private LinkedList<String> list;
+
+    Registrator(){
+        list = new LinkedList<>();
+    }
+
+    public LinkedList<String> getList() {
+        return list;
+    }
+
+    public void addList(String action){
+        list.add(action);
+    }
+
+    public void pushToFile(){
+        File file = new File("Registrator.txt");
+        FileOutputStream fos = null;
+        OutputStreamWriter in = null;
+        BufferedWriter buff = null;
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            fos = new FileOutputStream(file);
+            in = new OutputStreamWriter(fos);
+            buff = new BufferedWriter(in);
+
+            String result = "\n";
+            for (String str:list
+                 ) {
+                result+=str+" "+"\r\n";
+                }
+            buff.write(result);
+        }catch(IOException e){
+                e.printStackTrace();
+            }finally{
+                try{
+                    buff.close();
+            }catch(IOException e1){
+                    e1.printStackTrace();
+                }
+        }
+    }
+}
